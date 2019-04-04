8c753315fbba03f385ec37a98a16f6729a464e75
Durai <duraip@Ganesh-MacBookPro.local>
2019-04-04 17:59:41 +0530

    Initial commit

---
+++ ReactNativePlugin/ReactNativePlugin/ReactNativeBridge.swift
@@ -0,0 +1,17 @@
+//
+//  ReactNativeBridge.swift
+//  ReactNativePlugin
+//
+//  Created by Durai on 04/04/19.
+//  Copyright © 2019 Durai. All rights reserved.
+//
+
+import UIKit
+
+class ReactNativeBridge: NSObject {
+    
+    func testPod() -> Void {
+        print("Pod connection established successfully")
+    }
+
+}
---
+++ ReactNativePlugin/ReactNativePlugin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReactNativePlugin
+//
+//  Created by Durai on 04/04/19.
+//  Copyright © 2019 Durai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
